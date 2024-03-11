8d7c62f22a473e401dc9b3f94d379595c9c62538
abhi6774 <abhishekmourya6774@gmail.com>
2024-03-11 16:36:10 +0000

    Delete ModelFile and add chat functionality

---
+++ chat_session.py
@@ -0,0 +1,97 @@
+from typing import TypeVar, Generic
+from dataclasses import dataclass
+import requests
+import json
+from logger import log
+
+
+@dataclass
+class Message:
+    role: str
+    content: str
+    def __init__(self, content: str, role="user") -> None:
+        self.role = role
+        self.content = content
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+
+@dataclass
+class ChatResponse:
+    model: str
+    created_at: str
+    message: Message
+    done: bool
+    total_duration: int
+    load_duration: int
+    prompt_eval_count: int
+    prompt_eval_duration: int
+    eval_count: int
+    eval_duration: int
+
+    @classmethod
+    def from_dict(cls, data):
+        data['message'] = Message.from_dict(data['message'])
+        return cls(**data)
+
+
+class Chat:
+    _session_id: str
+    _chat_content: list[Message] = []
+    _model_name: str
+
+    def __init__(self, id: str, model_name: str = "llama2"):
+        self._session_id = id
+        self._model_name = model_name
+
+    def _get_model_response(self) -> ChatResponse:
+        url = "http://localhost:11434/api/chat"
+        data = {
+            "model": self._model_name,
+            "messages": self.get_messages(),
+            "stream": False
+        }
+        r = requests.post(url, json=data).json()
+        response = ChatResponse.from_dict(r)
+        log(response)
+        self._chat_content.append(Message(response.message.content, role=response.message.role))
+        return response
+
+    def send_message(self, inp: str, bot=False):
+        if bot:
+            self._chat_content.append(Message(role="assistant", message=inp))
+        self._chat_content.append(Message(inp))
+        return self._get_model_response()
+    
+    def get_messages(self):
+        return [message.__dict__ for message in self._chat_content]
+
+
+    def get_session_id(self):
+        return self._session_id
+
+class ChatSession:
+    _chat_sessions: list[Chat] = []
+
+    def get_chat_session(self, id: str, model_name: str = None):
+        for session in self._chat_sessions:
+            if id == session.get_session_id():
+                return session
+        return self.create_chat_session(id, model_name)
+    
+    
+    def create_chat_session(self, id: str, model_name: str = None):
+        for session in self._chat_sessions:
+            if session.get_session_id() == id:
+                return None
+        if model_name:
+            self._chat_sessions.append(Chat(id, model_name))
+        self._chat_sessions.append(Chat(id))
+        return self._chat_sessions[-1]
+
+
+if __name__=="__main__":
+    message = Message("Hi")
+    print(json.dumps(message))
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from summarizer import summarize_from_pdf, ollama_summarize, _summarize_text, extract_key_phrase
 from logger import log
+from chat_session import ChatSession
 
 app = FastAPI()
 
@@ -11,6 +12,8 @@ class Input(BaseModel):
 
 
 allowed_origins = ["*"]
+
+session = ChatSession()
 
 app.add_middleware(
     CORSMiddleware,
@@ -46,3 +49,27 @@ async def uploadAFile(file: UploadFile = File()):
         "key_entities": extract_key_phrase(summary["original_text"])
     }
 
+
+class ChatInput(BaseModel):
+    context: str | None
+    content: str | None
+
+@app.post("/chat/{chat_session}")
+def talk(chat_session: str, inp: ChatInput | None):
+    if not inp:
+        return {}
+    if inp.context:
+        rs = session.get_chat_session(chat_session, "qna_model")
+        mResponse = rs.send_message(inp.context)
+    else:
+        rs = session.get_chat_session(chat_session, "qna_model")
+        mResponse = rs.send_message(inp.content)
+    return  mResponse.message
+
+
+@app.post("/chat/r/{chat_session}")
+def talk(chat_session: str, inp: Input | None):
+    if not inp:
+        return {}
+    rs = session.get_chat_session(chat_session)
+    return rs.get_messages()
